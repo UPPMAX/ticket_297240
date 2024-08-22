3cb55eb36b89dfb076d3d5499bc730c3548f86a4
richelbilderbeek <richel@richelbilderbeek.nl>
2024-08-22 11:02:05 +0200

    Use local script

--- script.sh
+++ script.sh
@@ -5,6 +5,7 @@ module load cellranger/7.1.0
 
 export PYTHONPATH=/sw/bioinfo/Chromium-cellranger/7.1.0/bianca/lib/python/site-packages:$PYTHONPATH
 
-python3 /home/angee/Desktop/proj/data/ScRNA/demultiplexing/override_assign_features.py
+# python3 /home/angee/Desktop/proj/data/ScRNA/demultiplexing/override_assign_features.py
+python3 override_assign_features.py
 
 cellranger multi --id=demultiplexed_pool2_rerun --csv=5p_hashing_demux_pool_2_rerun_config.csv
